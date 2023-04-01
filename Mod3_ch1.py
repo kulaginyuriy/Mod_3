22b66c11e21d86e5e3895d10b19ddfbf8a7d477d
kulagin yuriy <kulagin8480@gmail.com>
2023-04-01 15:15:16 +0100

    Модуль 3 часть 2

--- Mod3_ch1.py
+++ Mod3_ch1.py
@@ -1,13 +1,11 @@
-x = float(input("есть столько денег="))
-y = float(input("Хочу, чтобы было ="))
-p = float( input("%="))
-time = y/(x*p/100)
-z =((x+(x*p/100))*p/100)
+x = float(input("Введите начальный вклад: "))
+p = float(input("Введите процентную ставку: "))
+y = float(input("Введите конечную сумму: "))
 
+years = 0
 while x < y:
-    print (f"Time deposit = :{int(x)}")
-    x += z
-print(f"Вклад достиг: {y} руб, за {int(time)} года/лет") 
+    x = float(x + x * p / 100)
+    print (f"Вклад составляет = :{round(x,2)}") 
+    years += 1
 
-
-  
+print("Количество лет:", years)
